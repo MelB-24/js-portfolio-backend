40a9498ff8c444ec414f3a8302085934922cd333
Melissa Bykersma <melissabykersma@gmail.com>
2020-04-07 15:13:49 +1000

    added nodemailer for contact form

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const AWS = require("aws-sdk")
 const fs = require("fs")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const nodemailer = require("nodemailer")
 require("dotenv").config()
 
 const Project = require("./models/project")
@@ -16,9 +17,9 @@ app.use(cors())
 
 const storage = multer.diskStorage({
     destination: "uploads/",
-    filename: function(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, file.originalname)
-    }
+    },
 })
 
 const upload = multer({ storage: storage })
@@ -26,7 +27,7 @@ const upload = multer({ storage: storage })
 AWS.config.update({
     accessKeyId: process.env.IAM_ACCESS_ID,
     secretAccessKey: process.env.IAM_SECRET,
-    region: "ap-southeast-2"
+    region: "ap-southeast-2",
 })
 
 const s3 = new AWS.S3()
@@ -34,7 +35,7 @@ const s3 = new AWS.S3()
 mongoose.connect(
     process.env.DB_URL,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    err => {
+    (err) => {
         if (err) {
             console.log(`Error from MongoDB: ${err}`)
         } else {
@@ -45,7 +46,7 @@ mongoose.connect(
 
 // PROJECTS
 app.get("/projects", (req, res) => {
-    Project.find().then(project => {
+    Project.find().then((project) => {
         res.json(project)
     })
 })
@@ -54,7 +55,7 @@ app.post("/new-project", (req, res) => {
     const { title, languages, githubLink, url, photoUrl } = req.body
 
     Project.create({ title, languages, githubLink, url, photoUrl }).then(
-        project => {
+        (project) => {
             res.json(project)
         }
     )
@@ -81,12 +82,12 @@ function uploadFile(source, targetName, res) {
             const putParams = {
                 Bucket: "portfolio-images-2410",
                 Key: targetName,
-                Body: filedata
+                Body: filedata,
             }
 
             s3.putObject(putParams, (err, data) => {
                 if (err) {
-                    console.log("Could nor upload the file. Error :", err)
+                    console.log("Could not upload the file. Error :", err)
                     return res.send({ success: false })
                 } else {
                     console.log("Successfully uploaded the file")
@@ -102,10 +103,10 @@ function uploadFile(source, targetName, res) {
 function retrieveFile(filename, res) {
     const getParams = {
         Bucket: "portfolio-images-2410",
-        Key: filename
+        Key: filename,
     }
 
-    s3.getObject(getParams, function(err, data) {
+    s3.getObject(getParams, function (err, data) {
         if (err) {
             return res.status(400).send({ success: false, err: err })
         } else {
@@ -113,3 +114,46 @@ function retrieveFile(filename, res) {
         }
     })
 }
+
+var transport = {
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+}
+
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+    if (!error) {
+        console.log("Server is ready to take messages")
+    } else {
+        console.log(error)
+    }
+})
+
+app.post("/send", (req, res, next) => {
+    const { name, email, message } = req.body
+
+    var content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+    var mail = {
+        from: name,
+        to: "melissabykersma@gmail.com", //Change to email address that you want to receive messages on
+        subject: "New Message from Contact Form",
+        text: content,
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                msg: "fail",
+            })
+        } else {
+            res.json({
+                msg: "success",
+            })
+        }
+    })
+})
